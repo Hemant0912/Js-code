99314db598c95a9e93e72fe25181e25bb1b5c457
Hemant Vishwakarma <134708926+Hemant0912@users.noreply.github.com>
2024-07-26 07:16:46 +0000

    creating of variables

---
+++ 01-basics/01-variables.js
@@ -0,0 +1,14 @@
+const accountId = 95697 // const keyword cannot be changed
+let accountEmail = "hem001122@google.com" 
+var accountPass = "12345"
+accountCity ="Vns"  // this way is not good to declare variable
+
+console.log(accountId); // to print 
+console.table([accountEmail , accountPass , accountCity]) /* to print in table form
+when we want to print multiple values
+*/
+/*
+we should use const and let keyword only because let
+and direct name gives issue in block scope and 
+functional scope
+*/
