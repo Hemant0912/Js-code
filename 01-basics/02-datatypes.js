16dc98419907d718492d04c375a6c6b9fa13dc7e
Hemant Vishwakarma <134708926+Hemant0912@users.noreply.github.com>
2024-07-26 07:50:32 +0000

    datatypes

---
+++ 01-basics/02-datatypes.js
@@ -0,0 +1,18 @@
+let name = "Hemant"  // this is string data type
+let age = 22;  // this is int data type
+let isLoggedIn = true; // this is boolean data type
+console.log(typeof age);
+
+
+//PRIMITIVE
+// number = to power 53
+// bigint = used in big pages like facebook, reddit etc
+// string = ""
+// boolean = t or f
+// null = standalone value
+// undefined = no value assisgned
+// symbol = it is used for uniqueness 
+
+// NON-PRIMITIVE
+// object
+// 
