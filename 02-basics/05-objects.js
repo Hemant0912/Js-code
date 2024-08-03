77d8cf7d54e31a6704d286cc2c9215cce5f16615
Hemant Vishwakarma <134708926+Hemant0912@users.noreply.github.com>
2024-08-03 15:15:23 +0000

    API and more

---
+++ 02-basics/05-objects.js
@@ -0,0 +1,42 @@
+const course = {
+    couresename: "alpha",
+    price: "999",
+    teacher: "Hemant"
+}
+
+const {teacher: guru} = course // teacher:guru is the destructuring
+console.log(guru); // i am calling guru instead of teacher 
+
+//----------------------------------------------------------------------//
+
+// API - simple bhasa m apna kaam kisi aur k sar par dalna
+// API - Application Programming Interface
+
+// In API we get values from backend and how we write that value
+// First we were getting value in form of XML structure which was difficult
+// Now we are getting value in form of JSON form 
+
+//JSON - format 
+
+// This is an API
+
+// {
+//     "name": "Hemant",
+//     "address": "Varanasi"
+// }
+
+// this is array type of API
+
+// [
+//     {}
+//     {}
+// ]
+
+
+
+
+
+
+
+
+
