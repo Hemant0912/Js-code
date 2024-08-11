838f61a414794de19c5bc68ee7e6dff80c1ce18d
Hemant Vishwakarma <134708926+Hemant0912@users.noreply.github.com>
2024-08-11 17:42:04 +0000

    while & do-while loop

---
+++ 05-loops.js/two.js
@@ -0,0 +1,26 @@
+// while loop
+
+let index =0   // init
+while (index <= 10) {  // condition
+    console.log(index);
+    index++   // updatation
+}
+
+//-------------------------------------------------------//
+
+let myArray = ["Hemant", "Hem", "Hemu", "Curry"]
+let i=0
+while(i<myArray.length) {
+    console.log(myArray[i]);
+    i++
+}
+
+// -------------------------------------------------------//
+
+// do-while loop
+
+let score = 1
+do {
+    console.log(`score is ${score}`);
+    score++
+} while (score <= 10);
