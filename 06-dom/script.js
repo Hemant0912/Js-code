e4c683b024bfa2537059567d8abc7f534e90b11c
Hemant Vishwakarma <134708926+Hemant0912@users.noreply.github.com>
2024-08-19 05:33:42 +0000

    Dom

---
+++ 06-dom/script.js
@@ -0,0 +1,36 @@
+/* right click and then open inspect go to console 
+ now to target any element write in console document.getElementById
+ (it is a method) so to access any element using write like this
+ document.getElementById('title')  
+
+to access element by class
+ document.getElementbyClassName('bg-black')               */
+
+//--------------------------------------------------------------------//
+
+ /* there are many ways to access element like 
+ document.getElementById('title').getAttributr('id) */
+
+/* to replace any id or class we use 
+document.getElementById('title').setAttribute('class', 'test') */
+
+//--------------------------------------------------------------------//
+
+/* how to see content 
+  title.textContent - it will show what is written on code if something
+  is hidden from screen but is on code it will show this
+
+  title.innerHTML - it will show the html code along with content
+
+  title.innerText - it will show what is visible on the screen */
+
+//--------------------------------------------------------------------//
+
+/* how to access element without writing  byId or byClassName
+  document.querySelector('.heading') 
+  query selector is a good way to select any element  */  
+
+/* how to access all element we use
+   document.querySelectorAll('p')   // to access all paragraph   */
+
+/* for more to learn just google it */
